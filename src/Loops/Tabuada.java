8a344a6b65af677d65dff8ea5388f18070038708
Daniela Gouvea <danielagouvea13@gmail.com>
2025-11-27 10:30:40 -0300

    segundo commit

--- src/Loops/Tabuada.java
+++ src/Loops/Tabuada.java
@@ -1,5 +1,23 @@
 package Loops;
 
+ import java.util.Scanner;
+
 public class Tabuada {
-    
+      public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+
+    int multiplicador;
+    int resultado;
+      
+        System.out.print("Digite o número do multiplicador da tabuada desejada: ");
+                multiplicador = scan.nextInt();
+        System.out.print("Tabuada do "  + multiplicador + ": ");
+        
+        for ( int i = 0;i <= 10; i++){
+            resultado = i * multiplicador;
+            System.out.print("\n- "  + multiplicador + " x "+ i + " = " + resultado);
+        }
+        
+
+    }
 }
